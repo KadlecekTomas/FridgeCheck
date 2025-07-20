f563fd789fedbc209d5141942c5eea0ce2dd8162
Tomas Kadlecek <kadle.tom@gmail.com>
2025-07-20 12:34:13 +0200

    Refactor: načítání Supabase z .env.local

---
+++ web/src/pages/login.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+import SignInWithGoogle from '../components/SignInWithGoogle'
+
+export default function LoginPage() {
+  return (
+    <div style={{ padding: 32 }}>
+      <h1>Login</h1>
+      <SignInWithGoogle />
+    </div>
+  )
+}
